2d884f6c39868b4582536c02483ea2390480afd9
Carl Parm <carlparm@gmail.com>
2020-01-15 19:04:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,4 +14,12 @@ def pop_with_args(array)
   return array.pop(2)
 end
 
-def using
+def using_shift(array)
+  return array.shift
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array, array2)
