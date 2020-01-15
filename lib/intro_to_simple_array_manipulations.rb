1e6a13d847dcac240fd3988ea267633f4bb8b2d6
Carl Parm <carlparm@gmail.com>
2020-01-15 19:09:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,4 +27,16 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, new_element)
+  array.insert(4, new_element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+def using_delete(array, string)
   
